2d41b24017f245c5042c8317e1765cc17fc74d4f
macnaer <trofimchuk.an@gmail.com>
2025-01-25 15:59:59 +0200

    Unlock Jenkins user

--- ToolBox/terraform/files/install_apps.sh
+++ ToolBox/terraform/files/install_apps.sh
@@ -1,6 +1,7 @@
-
 #! /bin/bash
 
+
+###################Install Jenkins######################
 sudo apt -y update
 sudo apt install -y openjdk-17-jre
 sudo apt install -y openjdk-17-jdk
@@ -12,10 +13,38 @@ sudo apt -y update
 sudo apt install -y jenkins
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
+#########################################################
 
 
+###################Unlock Jenkins###########################
+url="http://localhost:8080"
+password=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
+
+echo $password
+
+# NEW ADMIN CREDENTIALS URL ENCODED USING PYTHON
+username="master"
+new_password="Qwerty-1"
+fullname="Full name"
+email="hello@world.com"
+
+cookie_jar="$(mktemp)"
+full_crumb=$(curl -u "admin:$password" --cookie-jar "$cookie_jar" $url/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
+arr_crumb=(${full_crumb//:/ })
+only_crumb=$(echo ${arr_crumb[1]})
 
 
+# MAKE THE REQUEST TO CREATE AN ADMIN USER
+curl -X POST -u "admin:$password" $url/setupWizard/createAdminUser \
+        -H "Connection: keep-alive" \
+        -H "Accept: application/json, text/javascript" \
+        -H "X-Requested-With: XMLHttpRequest" \
+        -H "$full_crumb" \
+        -H "Content-Type: application/x-www-form-urlencoded" \
+        --cookie $cookie_jar \
+        --data-raw "username=$username&password1=$new_password&password2=$new_password&fullname=$fullname&email=$email&Jenkins-Crumb=$only_crumb&json=%7B%22username%22%3A%20%22$username%22%2C%20%22password1%22%3A%20%22$new_password%22%2C%20%22%24redact%22%3A%20%5B%22password1%22%2C%20%22password2%22%5D%2C%20%22password2%22%3A%20%22$new_password%22%2C%20%22fullname%22%3A%20%22$fullname%22%2C%20%22email%22%3A%20%22$email%22%2C%20%22Jenkins-Crumb%22%3A%20%22$only_crumb%22%7D&core%3Aapply=&Submit=Save&json=%7B%22username%22%3A%20%22$username%22%2C%20%22password1%22%3A%20%22$new_password%22%2C%20%22%24redact%22%3A%20%5B%22password1%22%2C%20%22password2%22%5D%2C%20%22password2%22%3A%20%22$new_password%22%2C%20%22fullname%22%3A%20%22$fullname%22%2C%20%22email%22%3A%20%22$email%22%2C%20%22Jenkins-Crumb%22%3A%20%22$only_crumb%22%7D"
+#################################################
+#####################Install Docker################
 sudo apt-get -y update
 sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -31,11 +60,12 @@ sudo apt-get -y update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo systemctl enable docker
-
+#########################################################
+########################Setup##########################
 sudo usermod -aG docker jenkins
 sudo usermod -aG adm jenkins
-sudo systemctl  restart jenkins
-
+sudo systemctl restart jenkins
 
 sudo mkdir /opt/mssql
-sudo chmod 777 /opt/mssql/
+sudo chmod 777 /opt/mssql
+########################################################
--- ToolBox/terraform/variables.tf
+++ ToolBox/terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "aws_access_key" {
   type    = string
-  default = ""
+  default = "AKIAXY7P3NWVDNZYF5UV"
 }
 
 variable "aws_secret_key" {
   type    = string
-  default = ""
+  default = "zbQpKKMJVC93Ggwde+y+XFOF6ci6djl0Kj2MIvVI"
 }
 
 variable "region" {
@@ -15,5 +15,5 @@ variable "region" {
 
 variable "public_ip" {
   type    = string
-  default = "eipalloc-03ba0d33dc828c5a5"
+  default = "eipalloc-0e1b775429cfb1803"
 }
